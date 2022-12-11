7e801c0032979ca34b4eadba87516f4ab82e816e
koya171 <narendrakoya09@gmail>
2022-12-12 00:04:02 +0530

    my final commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+require("express-async-errors");
+const app = express();
+
+app.use(express.json());
+mongoose.set("strictQuery", true);
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/product");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    // connectDB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ populate.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const Product = require("./models/product");
+const jsonProducts = require("./products.json");
+mongoose.set("strictQuery", true);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
+    console.log("Success!!!!");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
